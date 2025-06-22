04f2d4181d1b122381bf25dd44d8295c87418752
lucaslinharez <lucasgithubprojetos@gmail.com>
2025-06-22 19:16:40 -0300

    Create Aula 1
    Pasta com exercícios sobre função print

---
+++ Print.py
@@ -0,0 +1,13 @@
+""" A função print, recebe argumentos e mostra na tela"""
+
+print(1,2,3)
+
+""" Como padrão da função, o espaço já é inserido, mas, podemos usar sep= para escolher outro tipo de separador"""
+
+print(1,2,3, sep="-")
+
+""" Para quebra de linha ao final do código, usamos end=\n"""
+
+print(10, 9, 5, sep='-', end='\n123')
+
+"""As funções funcionam apenas com numeros"""
