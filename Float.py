27be2dbfffd5b078ed17b2df217a95c72e8e035f
lucaslinharez <lucasgithubprojetos@gmail.com>
2025-06-23 10:15:21 -0300

    Create Float

---
+++ Float.py
@@ -0,0 +1,34 @@
+# Tipos inteiros e flutuantes. Int e Float
+
+# INT -> número inteiro
+# O tipo int representa qualquer número, seja positivo ou negativo
+# Int sem sinal, é considerado positivo
+
+#Exemplos inteiros:
+print(10, 9, 8)
+print(-10, -9, -8)
+print(0)
+
+# FLOAT -> número com ponto flutuante 
+# O tipo float representa qualquer número com ponto flutuante, negativo ou positivo.
+# Float sem sinal, é considerado positivo.
+
+# Exemplos Float:
+print(1.1, 10.5, 0.0)
+
+# TYPE -> essa função/classe mostra o tipo que o Python inferiu o valor.
+
+# Exemplo Type: 
+print(type('Olá mundo!'))
+print(type(10))
+print(type(1.1))
+print(type('Olá'), (10), (1.1), (-0))
+
+# Resultado
+
+<class 'str'>
+<class 'int'>
+<class 'float'>
+<class 'str'> 10 1.1 0
+
+# Type é considerada uma classe, e não como função.
