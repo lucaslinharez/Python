dfac3e4ef7075f3fbf528e48f6094839baeecd8e
lucaslinharez <lucasgithubprojetos@gmail.com>
2025-06-23 09:25:27 -0300

    Create String.py

---
+++ Strings.py
@@ -0,0 +1,18 @@
+"""Strings são textos exibidos. Sempre utilizar aspas simples ou duplas"""
+
+"""Aspas simples: """
+print('Olá mundo!')
+
+"""Aspas duplas:"""
+print("Olá mundo!")
+
+"""Nesses formatos, o texto será exibido sem as aspas: Olá mundo!"""
+"""Para inserir aspas que apareçam ao usuário, usamos # Escape"""
+
+# Escape
+print("\"Olá mundo!\"")
+
+"""Podemos melhorar o código, usando aspas simples. Exemplo:"""
+print('"Olá, mundo!"')
+
+"""O processo pode ser feito ao contrário e não terá problemas"""
