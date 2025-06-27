27e7d400b932450359d764ffc21f8fa80b4e534f
lucaslinharez <lucasgithubprojetos@gmail.com>
2025-06-27 17:01:49 -0300

    Create Boolean

---
+++ Boolean.py
@@ -0,0 +1,9 @@
+# Função boolean
+
+print(10 == 10) # Sim = True (verdaeiro)
+print(10 == 11) # Não = False (falso)
+print(type(10 == 11)) # Para saber o tipo de dado
+
+# Retorna: True 
+#          False
+#          <class 'bool'>
