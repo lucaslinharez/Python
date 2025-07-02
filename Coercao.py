20d8083da3b861ec4443708951f1fff3b00e240d
lucaslinharez <lucasgithubprojetos@gmail.com>
2025-07-02 11:28:25 -0300

    Create Coercao.py

---
+++ Coercao.py
@@ -0,0 +1,24 @@
+# Coerção Implícita, é feita automaticamente pelo interpretador do Python
+# Quando dois types compativeis são usados na mesma operação.
+
+a = 5
+b = 2.0
+c = a + b
+print(c)
+print(type(c))
+
+# Coerção Explícita (casting) é feita manualmente pelo programador
+# utilizando funções como int(), float(), str(), entre outras
+
+x = "10"
+y = 5
+z = int(x) + y  # Faz a conversão explícita de string para inteiro
+print(type(z))  # Saída mostrando o type convertido
+
+# Importante: 
+# Coerção implícita só ocorre entre tipos compatíveis (por exemplo: int e float).
+# Python é fortemente tipado, então não força coerções arriscadas ou incoerentes automaticamente (como somar int com str, o que dá erro).
+# A coerção explícita te dá mais controle, mas você deve garantir que o valor seja realmente compatível. 
+# Exemplo:
+
+int("abc")  # Vai gerar ValueError
