020f3edeb3d608cb41cfc4237a96742166abd833
lucaslinharez <lucasgithubprojetos@gmail.com>
2025-07-04 15:50:33 -0300

    Create Concatenacao.py

---
+++ Concatenacao.py
@@ -0,0 +1,15 @@
+# Concatenação em Python, é o processo de juntar ou combinar valores
+# Geralmente entre strs, em uma única sequência. A forma mais comum é usar o operador +
+
+# Exemplo de concatenação
+
+concatenacao = 'Texto' + 'Texto'  # somente strings
+print(concatenacao)
+
+# Exemplo de repetição, onde o operador * é usado
+
+teste1 = 'A' * 5
+teste2 = '3 vezes' * 3            # numeros inteiros
+print(teste1)
+print(teste2)
+
