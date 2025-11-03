05c71b18619e6c5eae7418242616fd94497e6337
lucaslinharez <lucasgithubprojetos@gmail.com>
2025-11-03 12:55:58 -0300

    Create Manipulação-de-Dados.py

---
+++ Introdução-Ciencia-Dados/Manipulação-de-Dados.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+
+uri = "https://raw.githubusercontent.com/alura-cursos/introducao-a-data-science/refs/heads/master/aula4.1/movies.csv"
+pd.read_csv(uri)
+
+### colocar apeliado para o pandas
