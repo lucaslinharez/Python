c4db27d70b9b97e2c0e67a46a055e1a448125ad0
lucaslinharez <lucasgithubprojetos@gmail.com>
2026-01-06 19:37:34 -0300

    Update Formatacao-String.py
    Função Len

--- Formatacao-String.py
+++ Formatacao-String.py
@@ -13,10 +13,20 @@
 
  """
 
-
 variavel = 'ABC'
 print(f'{variavel}')
 print(f'{variavel: >10}')
 print(f'{variavel: <10}')
 print(f'{variavel: ^10}')
 
+""" 
+
+Função len conta a quantidade de caracteres usadas nas strings, incluindo campos vazios
+Funciona além de strings
+
+"""
+
+# Exemplo:
+
+variavel = 'Olá mundo'
+print(len(variavel))
