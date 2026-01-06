a7624cdb36af272a5d291cda8f74a8862c9c153a
lucaslinharez <lucasgithubprojetos@gmail.com>
2026-01-06 19:25:18 -0300

    Create

---
+++ Formatacao-String.py
@@ -0,0 +1,22 @@
+
+# Formatação básica de String
+
+""" 
+
+ s - string 
+ d - int
+ f - float
+ > - esquerda
+ > - direita
+ ^- centro
+ 
+
+ """
+
+
+variavel = 'ABC'
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{variavel: <10}')
+print(f'{variavel: ^10}')
+
